ad292f2b15a360376fd75d453dc2330f46b2afec
td59210 <td59210@mail.harpercollege.edu>
2026-02-01 00:15:41 -0600

    update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <array>
+#include <vector>
 
 using namespace std;
 
@@ -30,19 +32,69 @@ int maximum() {
 
 	//max in an array
 
-	const int ARR_SIZE = 5;
-	int arr[ARR_SIZE] = { 3, 14, 76, 11, 26 };
+	int arr[] = {3, 14, 76, 11, 26};
 	int* max_ptr = arr;
 	
-	for ()
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	for (int i = 0; i < size; i++) { //loops through as long as less than size of array
+
+		if (arr[i] > *max_ptr) {
+
+			max_ptr = &arr[i]; //sets new value as max
+		}
+
+	}
+
+	return *max_ptr;
+}
+
+int length() {
+	//calc num of characters in string
+
+	char string[] = "computer";
+	char* num_ptr = string;
+	int i = 0;
+
+		while (*num_ptr != '\0') {
+
+			i++;
+
+			num_ptr = &string[i];
+	}
+		
+		return i;
+}
+
+int reverse() {
+	//print reverse of string
+
+	char string[] = "bottle";
+	char* norm_ptr = string;
+	int i = 0;
+
+	while (*norm_ptr != '\0') {
+
+		i++;
+
+		norm_ptr = &string[i];
+	}
 	
 
+
 }
+
 
 int main() {
 
 	derefrencing();
 
 	maximum();
+	cout << "\nMax value is " << maximum() << endl;
+
+	length();
+	cout << "Length of string is " << length() << endl;
+
+	reverse();
 	
 }
